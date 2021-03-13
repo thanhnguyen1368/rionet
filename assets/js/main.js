845fe409daeda1f62d2b821160d48cf150d1d8b7
cuong nguyen <cuongnguyend863@gmail.com>
2021-03-13 22:00:34 +0700

    create core about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,53 @@
 var isClicked = false;
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-  /*----Slick slider----*/
-  $('.slider-login').slick({
-		centerMode: false,
-		slidesToShow: 1,
+    /*----Slick slider----*/
+    $('.slider-login').slick({
+        centerMode: false,
+        slidesToShow: 1,
         arrows: false,
         autoplay: true,
         dots: true
-	});
+    });
+    $('.slider-about').slick({
+        centerMode: false,
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        dots: true
+    });
 
 
-  /*----Back to Top----*/
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 100) {
-			$('.pagetop').addClass('active');
-		} else {
-			$('.pagetop').removeClass('active');
-		}
-	});
+    /*----Back to Top----*/
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.pagetop').addClass('active');
+        } else {
+            $('.pagetop').removeClass('active');
+        }
+    });
 
-	$(".pagetop a").click(function() {
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-		return false;
-	});
+    $(".pagetop a").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 
 
-  $(document).click(function() {
-		if (!isClicked) {
-			$('.languages ul').removeClass('dropdown-languages');
-		}
-		isClicked = false;
-	});
+    $(document).click(function() {
+        if (!isClicked) {
+            $('.languages ul').removeClass('dropdown-languages');
+        }
+        isClicked = false;
+    });
 
 });
 
-var wow = new WOW(
-  {
-    boxClass:     'wow',
+var wow = new WOW({
+    boxClass: 'wow',
     animateClass: 'animated',
-    offset:       0,
-    mobile:       true,
-    live:         true
-  }
-);
+    offset: 0,
+    mobile: true,
+    live: true
+});
 wow.init();
